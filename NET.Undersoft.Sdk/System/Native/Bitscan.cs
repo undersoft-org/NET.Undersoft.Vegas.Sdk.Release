5c49f3208e3fe78e7dfeb3631142340b1f1c7558
daha <dh@undersoft.pl>
2021-04-19 04:53:33 +0200

    U

--- NET.Undersoft.Sdk/System/Native/Bitscan.cs
+++ NET.Undersoft.Sdk/System/Native/Bitscan.cs
@@ -7,28 +7,28 @@ namespace System
 {
     public static class Bitscan
     {
-        [DllImport("Undersoft.System.Native.Bitscan.dll", CallingConvention = CallingConvention.Cdecl)]
+        [DllImport("Undersoft.System.Native.Submix.dll", CallingConvention = CallingConvention.Cdecl)]
         public static extern uint ReverseIndex32(uint x);
 
-        [DllImport("Undersoft.System.Native.Bitscan.dll", CallingConvention = CallingConvention.Cdecl)]
+        [DllImport("Undersoft.System.Native.Submix.dll", CallingConvention = CallingConvention.Cdecl)]
         public static extern uint ForwardIndex32(uint x);
 
-        [DllImport("Undersoft.System.Native.Bitscan.dll", CallingConvention = CallingConvention.Cdecl)]
+        [DllImport("Undersoft.System.Native.Submix.dll", CallingConvention = CallingConvention.Cdecl)]
         public static extern uint LengthBefore32(uint x);
 
-        [DllImport("Undersoft.System.Native.Bitscan.dll", CallingConvention = CallingConvention.Cdecl)]
+        [DllImport("Undersoft.System.Native.Submix.dll", CallingConvention = CallingConvention.Cdecl)]
         public static extern uint LengthAfter32(uint x);
 
-        [DllImport("Undersoft.System.Native.Bitscan.dll", CallingConvention = CallingConvention.Cdecl)]
+        [DllImport("Undersoft.System.Native.Submix.dll", CallingConvention = CallingConvention.Cdecl)]
         public static extern uint ReverseIndex64(ulong x);
 
-        [DllImport("Undersoft.System.Native.Bitscan.dll", CallingConvention = CallingConvention.Cdecl)]
+        [DllImport("Undersoft.System.Native.Submix.dll", CallingConvention = CallingConvention.Cdecl)]
         public static extern uint ForwardIndex64(ulong x);
 
-        [DllImport("Undersoft.System.Native.Bitscan.dll", CallingConvention = CallingConvention.Cdecl)]
+        [DllImport("Undersoft.System.Native.Submix.dll", CallingConvention = CallingConvention.Cdecl)]
         public static extern uint LengthBefore64(ulong x);
 
-        [DllImport("Undersoft.System.Native.Bitscan.dll", CallingConvention = CallingConvention.Cdecl)]
+        [DllImport("Undersoft.System.Native.Submix.dll", CallingConvention = CallingConvention.Cdecl)]
         public static extern uint LengthAfter64(ulong x);
     }
 }
